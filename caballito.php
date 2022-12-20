7d3e9ed9b06e51830df5dacd1f32a6d889260df5
Pablo Lires <pablolires@gmail.com>
2022-12-20 18:47:20 -0300

    agregado de pixel de facebook

--- caballito.php
+++ caballito.php
@@ -7,6 +7,10 @@
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
+
+    <!-- Pixel Facebook -->
+    <?php include('includes/pixel-facebook.php'); ?>
+    
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="S2D Emprendimientos Inmobiliarios SRL nace en respuesta a las nuevas necesidades y posibilidades del mercado, ofreciendo el más alto nivel en innovación y estrategia inmobiliarios de primera calidad. Proyecto Caballito">
 
